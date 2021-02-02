b8a149222e2dd9322cb17920f7d79bcdc6cd659c
Ababei Edoardo <e.ababei.1257@vallauri.edu>
2021-02-02 18:38:49 +0100

    Aggiunto esercizio 3 automobili ajax

---
+++ Ajax/Es03-Automobili/index.js
@@ -0,0 +1,17 @@
+"option strict"
+
+const URL = "http://localhost:3000"
+
+$(document).ready(function () {
+    let _lstMarche = $("#lstMarche");
+    let _lstModelli = $("#lstModelli");
+	let _table= $("table")
+	let _dettagli=$(".row").eq(2).children("div").eq(1)
+  
+    _dettagli.hide()
+       
+
+		
+});
+
+
