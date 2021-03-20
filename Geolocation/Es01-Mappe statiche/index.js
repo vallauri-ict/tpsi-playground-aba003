a24857cfdb1a9e4af0a23e583bf2bbab7f42c416
Ababei Edoardo <e.ababei.1257@vallauri.edu>
2021-03-20 12:15:38 +0100

    Aggiunto esercizio funzioante

--- Geolocation/Es01-Mappe statiche/index.js
+++ Geolocation/Es01-Mappe statiche/index.js
@@ -1,53 +1,60 @@
-"use strict";
+"use strict"
 
-const url = "https://maps.googleapis.com/maps/api/staticmap";
-const key = "";
+const URL = "https://maps.googleapis.com/maps/api/"
+const key  = "" 
 
 const params = {
-  //parametri comuni
-  key: key,
-  center: "44.5557763,7.7347183", //"si usa per le appe statiche
-  zoom: 16,
-  size: "800x600",
+    "key":key,
+    "center": "via san michele 68, fossano", //"44.5557763,7.7347183",
+    "zoom":16,  //valore intermedio, fra i più usati
+    "size":"800x600",
+    // maptype viene aggiunto dopo  manualmente
+    "markers":"color:blue|size:big|label:V|via san michele 68, fossano",    //mette un marcatore di colore blu all'indirizzo indicato
+    "heading" : "60",
+    "pitch" : "7",
+    "fov" : "45"
+}
+const mapType = ['roadmap', 'satellite', 'hybrid', 'terrain', 'streetview']; //vettore che da i vari tipi di visualizzazione
 
-  //si usa per le mappe streetviw
-  location:"44.5557763,7.7347183",
-   markers: "color:blue|size:big|label:V|via san michele 68, fossano",
-   heading:"-60",
-   pitch:"7",
-   fov:"45",
-};
-const mapType = ["roadmap", "satellite", "hybrid", "terrain", "streetview"];
+
 
 window.onload = function () {
-  let imgBox = $("#imgBox");
-  let btnBox = $("#btnBox");
+    let imgBox = $("#imgBox");
+    let btnBox = $("#btnBox");
 
-  for (const item of mapType) {
-    let btn = $("<button>");
-    btn.text(item);
-    btn.appendTo(btnBox);
-    btn.on("click", visualizzaMappa);
-  }
-
-  function visualizzaMappa() {
-    let url;
-    if ($(this).text() != "streetview") {
-      imgBox.prop("src", url +"?"+ setParams($(this).text()));
-
-    }else{
-      url=URL+"/streetview?"+setParams("streetview");
-      imgBox.prop("src", url +"?"+ setParams($(this).text()));
+    for (const type of mapType) 
+    {
+        let button = $("<button>");
+        button.text(type);
+        button.appendTo(btnBox);
+        button.on("click",visualizzaMappa);
     }
-  }
 
-  function setParams(maptype) {
-    let queryString = "";
-    for (const key in params) {
-      // se rimane una & al fondo non succede nulla
-      queryString += key + "=" + params[key] + "&";
+
+
+    function visualizzaMappa(){
+        let url;
+        if($(this) != "streetview")
+        {
+            url = URL + "staticmap?" + setParameters($(this).text());
+        }
+        else
+        {
+            url = URL +"streetview?" + setParameters("streetview");
+        }
+        console.log(url);
+        imgBox.prop("src", url);
     }
-    queryString += "maptype=" + maptype;
-    return queryString;
-  }
-};
+
+    function setParameters(mapType){
+        let qstring = "";
+        for (const key in params) 
+        {
+            qstring += key + "=" +params[key] + "&";
+        }
+        qstring += "maptype=" + mapType;
+
+        return qstring;
+    }
+
+}
