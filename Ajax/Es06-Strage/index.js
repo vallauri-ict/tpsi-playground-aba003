6256b0dab76b42d0713d0be5c85e4046a34accbe
Ababei Edoardo <e.ababei.1257@vallauri.edu>
2021-04-20 20:53:39 +0200

    Aggiunto eserczio 6 ajax strage con registrazione e dimentica password

--- Ajax/Es06-Strage/index.js
+++ Ajax/Es06-Strage/index.js
@@ -1,126 +1,220 @@
-"use strict"
+"use strict";
 
 $(document).ready(function () {
+  let _login = $("#login");
+  let _test = $("#test");
+  let voto = 0;
+  let _txtUsr = $("#usr");
+  let _txtPwd = $("#pwd");
+  let _btnLogin = $("#btnLogin");
+  let _lblErrore = $("#lblErrore");
+  let currentUser = "";
+  let registrazione = $("#registrazione");
 
-    let _login = $("#login")
-    let _test = $("#test")
-   
-    let _txtUsr = $("#usr")
-    let _txtPwd = $("#pwd")
-    let _btnLogin = $("#btnLogin")
-    let _lblErrore = $("#lblErrore")
-	
-	let _domande = $(".domande")
-	
-	/* ******************************* */
+  let formRegistrati = $("#formRegistrati");
+  let _txtUsrRegistrazione = $("#usrRegistrazione");
+  let _txtPwdRegistrazione = $("#pwdRegistrazione");
+  let _txtPwdConferma = $("#pwdRegistrazioneConferma");
 
-    _login.show()
-    _test.hide()
-    _lblErrore.hide()
-    let idStudente;
-   
-    _btnLogin.on("click", function(){
-       let user = _txtUsr.val();
-       let pwd = _txtPwd.val();
-       let json = {
-           "user":user,
-           "pwd":pwd
-       }
-       // uguale a quella sotto: let request = inviaRichiesta("get",`/studenti?user=${user}&pwd=${pwd}`); 
-       let request = inviaRichiesta("get","/studenti",json); // più strutturata
-       request.fail(errore);
-       request.done(function(data){
-           // data è un vettore che può essere lungo 1 se log in ok oppure 0 se login non è ok
-           console.log(data);
-           if(data.length == 0)
-                _lblErrore.fadeIn(600);
-            else
-            {
-                _login.hide();
-                _test.show();
-                idStudente = data[0].id;
-                InviaRichiestaDomande();
-            }
-       })
-    })
+  let _btnRegistrati = $("#btnRegistrati");
+  var regex = /^[a-zA-Z0-9_-]{5,15}$/;
+  let btnReset = $("#resetpw");
+  let _txtRipristinausr = $("#usrRipristina");
+  let _txtRipristinapwd = $("#pwdRipristina");
+  let _txtRipristinapwdconferma = $("#pwdconfermaripristino");
+  let btnConfermaCambioPwd = $("#btnConfermapwd");
+  let formRipristina = $("#formRipristina");
+  /* ******************************* */
+  _login.show();
+  _test.hide();
+  _lblErrore.hide();
+  formRegistrati.hide();
+  formRipristina.hide();
 
-    _lblErrore.children("button").on("click", function(){
-		_lblErrore.fadeOut(600)
-	})
-	/* ***************************************** */
-    function InviaRichiestaDomande(){
-        let request = inviaRichiesta("get","/domande");
-        request.fail(errore);
-        request.done(function(domande){
-            for (const domanda of domande) {
-                let br = $("<br>");
-                br.appendTo(_test.children().eq(2));
-
-                let div = $("<div>");
-                div.appendTo(_test.children().eq(2));
-
-                let p = $("<p>");
-                p.addClass("domanda");
-                p.text(domanda.domanda);
-                p.prop("id",domanda.id);
-                //p.appendTo(_domande);
-                p.appendTo(div);
-
-                let requestRisposte = inviaRichiesta("get","/risposte?codDomanda=" + domanda.id);
-                requestRisposte.fail(errore);
-                requestRisposte.done(function(risposte){
-                    for (const risposta of risposte) {
-                        let opt = $("<input type=radio>");
-                        opt.prop("risposta",risposta);
-                        opt.prop("name",domanda.id);
-                        opt.appendTo(div);
-
-                        let span = $("<span>");
-                        span.text(" " + risposta.risposta);
-                        span.appendTo(div);
-                        let br = $("<br>");
-                        br.appendTo(div);
-                    }
-                    let opt = $("<input type=radio>");
-                    opt.prop("risposta",{"correct":false});
-                    opt.prop("name",domanda.id);
-                    opt.prop("checked",true);
-                    opt.appendTo(div);
-
-                    let span = $("<span>");
-                    span.text(" Non rispondo");
-                    span.appendTo(div);
-
-                    let br = $("<br>");
-                    br.appendTo(div);
-                })
-            }
-            let button = $("<button>");
-            button.appendTo(_test.children().eq(2));
-            button.text("INVIA");
-            button.on("click",function(){
-                let _opts = $("input[type=radio]:checked");
-                let voto = 0;
-                // for (const opt of _opts) {
-                //     if($(opt).prop("risposta").correct == true)
-                //         voto++;
-                //     else
-                //         $(opt).next().css("color","red");
-                // }
-                _opts.each(function(i,ref){
-                    if($(this).prop("risposta").correct == true)
-                        voto++;
-                    else
-                        $(ref).next().css({"color":"red"});
-                })
-                let request = inviaRichiesta("patch","/studenti/" + idStudente, {"voto":voto});
-                request.fail(errore);
-                request.done(function(data){
-                    console.log(data);
-                    alert("Complimenti hai preso un bel " + voto);
-                })
-            })
-        })   
+  registrazione.click(function () {
+    _login.hide();
+    formRegistrati.show();
+    _txtUsrRegistrazione.val("");
+    _txtPwdConferma.val("");
+    _txtPwdRegistrazione.val("");
+  });
+  _btnRegistrati.click(function () {
+    if (_txtUsrRegistrazione.val().length > 0) {
+      let request = inviaRichiesta(
+        "get",
+        `/studenti?user=${_txtUsrRegistrazione.val()}`
+      );
+      request.fail(errore);
+      request.done(function (userName) {
+        console.log(userName);
+        if (userName.length > 0) {
+          alert("Nome utente non disponibile");
+          _txtUsrRegistrazione.css({ color: "red" });
+        } else {
+          _txtUsrRegistrazione.css({ color: "green" });
+        }
+      });
     }
+    if (
+      regex.test(_txtPwdRegistrazione.val()) &&
+      regex.test(_txtPwdConferma.val()) &&
+      _txtPwdConferma.val() == _txtPwdRegistrazione.val()
+    ) {
+      _txtUsrRegistrazione.css({ color: "green" });
+      _txtPwdRegistrazione.css({ color: "green" });
+      _txtPwdConferma.css({ color: "green" });
+      let request = inviaRichiesta("get", "/studenti");
+      request.fail(errore);
+      request.done(function (studenti) {
+        let request = inviaRichiesta("post", "/studenti", {
+          id: studenti.length + 1,
+          user: _txtUsrRegistrazione.val(),
+          pwd: _txtPwdRegistrazione.val(),
+          voto: 0,
+        });
+        request.fail(errore);
+        request.done(function name(params) {
+          alert("Dati inseriti correttamente");
+          formRegistrati.hide();
+          _login.show();
+        });
+      });
+    } else {
+      alert("Password errate o non valide");
+    }
+  });
 
+  _txtUsrRegistrazione.on("keyup", function () {
+    _txtUsrRegistrazione.css({ color: "gray" });
+  });
+
+  btnReset.click(function () {
+    _login.hide();
+    formRipristina.show();
+    _txtRipristinapwd.val("");
+    _txtRipristinausr.val("");
+    _txtRipristinapwdconferma.val("");
+  });
+  btnConfermaCambioPwd.click(function name(params) {
+    let user = _txtRipristinausr.val();
+    let password = _txtRipristinapwd.val();
+    let newpassword = _txtRipristinapwdconferma.val();
+    let request = inviaRichiesta(
+      "GET",
+      `/studenti?user=${user}&pwd=${password}`
+    );
+    request.fail(errore);
+    request.done(function (user) {
+      if (user.length == 0) {
+        alert("Username non esistente");
+      } else if (password == newpassword) {
+        alert("La password nuova non può essere uguale alla precedente");
+      } else if (
+        password != newpassword &&
+        user.length > 0 &&
+        regex.test(_txtRipristinapwdconferma.val())
+      ) {
+        currentUser = user;
+        let request = inviaRichiesta("patch", `/studenti/${user[0].id}`, {
+          pwd: newpassword,
+        });
+        request.fail(errore);
+        request.done(function name(params) {
+          alert("Password cambiata correttamente");
+          formRipristina.hide();
+          _login.show();
+        });
+      } else {
+        alert("Nuova password mancante o errata");
+      }
+    });
+  });
+  _btnLogin.click(function () {
+    let password = _txtPwd.val();
+    let user = _txtUsr.val();
+    let request = inviaRichiesta(
+      "GET",
+      `/studenti?user=${user}&pwd=${password}`
+    );
+    request.fail(errore);
+    request.done(function (user) {
+      if (user.length == 0) {
+        _lblErrore.show(200);
+      } else {
+        alert("Benvenuto " + user[0].user);
+        currentUser = user;
+        caricadomande();
+      }
+    });
+  });
+
+  function caricadomande() {
+    _login.hide();
+    _test.show();
+    let request = inviaRichiesta("get", "/domande");
+    request.fail(errore);
+    request.done(function (domande) {
+      for (const question of domande) {
+        let divDomanda = $("<div>");
+        divDomanda.appendTo(_test);
+
+        let domanda = $("<div>");
+        domanda.text(question.domanda);
+        domanda.appendTo(divDomanda);
+        let style = {
+          color: "blue",
+          "font-size": "20px",
+        };
+        domanda.css(style);
+        let br = $("<br>");
+        br.appendTo(divDomanda);
+        let request = inviaRichiesta(
+          "get",
+          `/risposte?codDomanda=${question.id}`
+        );
+        request.fail(errore);
+        request.done(function (answers) {
+          for (const answear of answers) {
+            let radio = $("<input>");
+            radio.prop("type", "radio");
+            radio.prop("name", `opt${answear.codDomanda}`);
+            radio.prop("radio", answear);
+            radio.appendTo(divDomanda);
+            let span = $("<span>");
+            span.text(answear.risposta);
+            span.appendTo(divDomanda);
+            let br = $("<br>");
+            br.appendTo(divDomanda);
+          }
+        });
+      }
+      let br = $("<br>");
+      br.appendTo(_test);
+      let button = $("<button>");
+      button.text("Verifica");
+      button.appendTo(_test);
+      button.click(function () {
+        let opts = $("input[type=radio]:checked");
+        for (const opt of opts) {
+          if ($(opt).prop("radio").correct == true) {
+            voto++;
+            $(opt).next().css({ color: "green" });
+          } else {
+            $(opt).next().css({ color: "red" });
+          }
+        }
+
+        let request = inviaRichiesta(
+          "patch",
+          `/studenti/${currentUser[0].id}`,
+          { voto: voto }
+        );
+        request.fail(errore);
+        request.done(function () {
+          alert(`Il tuo voto è: ${voto}`);
+          button.prop("disabled", true);
+        });
+      });
+    });
+  }
 });
