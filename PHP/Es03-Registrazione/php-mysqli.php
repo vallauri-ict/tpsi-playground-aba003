5767d2ebdf5a6acb48e56f7c7451dae5ac31a4ca
Ababei Edoardo <e.ababei.1257@vallauri.edu>
2021-04-27 18:12:22 +0200

    Revisionato ex 3 php procedure mysqli connection and execute

--- PHP/Es03-Registrazione/php-mysqli.php
+++ PHP/Es03-Registrazione/php-mysqli.php
@@ -1,5 +1,6 @@
 <?php
-function _connection($dbName){
+function _connection($dbName)
+{
 
     define('DBHOST', 'localhost');
     define('DBUSER', 'root');
@@ -7,32 +8,32 @@ function _connection($dbName){
 
     //questa riga fa in modo che in caso di errore venga generato una eccezione
     mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
-    try
-    {
-    $con = new mysqli(DBHOST, DBUSER, DBPASS, $dbName); 
-    return $con;
+    try {
+        $con = new mysqli(DBHOST, DBUSER, DBPASS, $dbName);
+        return $con;
+    } catch (mysqli_sql_exception $ex) {
+        die("Errore connessione db: <br>" . $ex->getMessage());
     }
-    catch (mysqli_sql_exception $ex) {
-    die ("Errore connessione db: <br>" . $ex->getMessage()); }
 }
 
-function execute($con, $sql){
- //query di tipo dml restituiscono true o false
- //query record set con dati
-   try{
-        $rs=$con->query($sql);
-        
-   }  catch (mysqli_sql_exception $ex) {
-    die ("Errore connessione db: <br>" . $ex->getMessage()); }
+function execute($con, $sql)
+{
+    //query di tipo dml restituiscono true o false
+    //query record set con dati
+    try {
+        $rs = $con->query($sql);
+    } catch (mysqli_sql_exception $ex) {
+        die("Errore connessione db: <br>" . $ex->getMessage());
+    }
 
     //il comando  era una query di tipo select
     //convertiamo il record set in un vettore json
     //i comandi non ti tipo select restituiscono un booleano che lasciamo cosi com è
     //altrimenti lo lasciamo così
     if (!is_bool($rs)) {
-        $data =$rs->fetch_all(MYSQLI_ASSOC);//gli dice di convertirlo in un vettore enumerativo di json
-    }else{
-        $data=$rs;
+        $data = $rs->fetch_all(MYSQLI_ASSOC); //gli dice di convertirlo in un vettore enumerativo di json
+    } else {
+        $data = $rs;
     }
     return $data;
 }
